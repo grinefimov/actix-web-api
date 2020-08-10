a30332a0cb72cae4e6b81994cac84e51cc0c92c5
Grigorii Efimov <grinefimov@gmail.com>
2020-08-10 23:14:37 +0300

    Setup Actix web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{middleware, web, App, HttpResponse, HttpServer, Responder, Result};
+
+async fn index() -> Result<impl Responder> {
+    //println!("GET: /");
+    Ok(HttpResponse::Ok().body("Hello world!"))
+}
+
+async fn again() -> Result<impl Responder> {
+    //println!("GET: /again");
+    Ok(HttpResponse::Ok().body("Hello world again!"))
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/").to(index))
+            .service(web::resource("/again").to(again))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
