fb6ff155d7cd742a2a0e322effa87e21aeaa2498
Grigorii Efimov <grinefimov@gmail.com>
2020-08-17 23:42:27 +0300

    Add app mod

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,10 @@
-use actix_web::{middleware, web, App, HttpResponse, HttpServer, Responder, Result};
-
-async fn index() -> Result<impl Responder> {
-    //println!("GET: /");
-    Ok(HttpResponse::Ok().body("Hello world!"))
-}
-
-async fn again() -> Result<impl Responder> {
-    //println!("GET: /again");
-    Ok(HttpResponse::Ok().body("Hello world again!"))
-}
+mod app;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
+    std::env::set_var("RUST_LOG", "actix_web=info"); //my_errors=debug,
+    std::env::set_var("RUST_BACKTRACE", "1");
     env_logger::init();
 
-    HttpServer::new(|| {
-        App::new()
-            .wrap(middleware::Logger::default())
-            .service(web::resource("/").to(index))
-            .service(web::resource("/again").to(again))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    app::start().await
 }
